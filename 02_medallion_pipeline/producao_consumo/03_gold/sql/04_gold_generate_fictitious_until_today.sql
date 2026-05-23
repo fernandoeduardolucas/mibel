4b09ec683319e3d2f6e196f2a0da6a1ea0e56b7a
fernandoeduardolucas <fernandoeduardolucas@icloud.com>
2026-05-23 10:19:34 +0100

    Fix Trino modulo type error in fictitious gold generator

--- 02_medallion_pipeline/producao_consumo/03_gold/sql/04_gold_generate_fictitious_until_today.sql
+++ 02_medallion_pipeline/producao_consumo/03_gold/sql/04_gold_generate_fictitious_until_today.sql
@@ -95,7 +95,7 @@ synthetic AS (
                 1.04
                 + 0.05 * sin(2 * pi() * ((hour(timestamp_utc) + 6) / 24.0))
                 + 0.03 * (
-                    (xxhash64(to_utf8(CAST(timestamp_utc AS varchar))) % 1000) / 1000.0
+                    (abs(from_big_endian_64(xxhash64(to_utf8(CAST(timestamp_utc AS varchar))))) % 1000) / 1000.0
                     - 0.5
                 )
             )
